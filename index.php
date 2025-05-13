556f7cf2e06c2a66d0da30588269b16f3f75458a
ionvop <ionvop@gmail.com>
2025-05-13 23:41:48 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+include "common.php";
+$db = new SQLite3("database.db");
+
+?>
+
+<html>
+    <head>
+        <title>
+            Login
+        </title>
+        <base href="./">
+        <link rel="stylesheet" href="style.css">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div class="main">
+            
+        </div>
+        <script src="script.js"></script>
+        <script>
+
+        </script>
+    </body>
+</html>
