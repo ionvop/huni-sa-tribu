8ac79799989d4860238dc2c5dca3640c205f755b
ionvop <ionvop@gmail.com>
2025-08-25 23:27:20 +0800

    Update init.php

--- init.php
+++ init.php
@@ -10,6 +10,7 @@ $db = new SQLite3("database.db");
 $query = <<<SQL
     CREATE TABLE "users" (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `fullname` TEXT, `email` TEXT UNIQUE, `username` TEXT UNIQUE, `hash` TEXT, `session` TEXT, `time` INTEGER DEFAULT (unixepoch()));
     CREATE TABLE "email_verifications" (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `email` TEXT, `code` TEXT, `is_verified` TEXT DEFAULT 'no', `time` INTEGER DEFAULT (unixepoch()));
+    CREATE TABLE `uploads` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `user_id` INTEGER REFERENCES `users`(`id`), `title` TEXT, `tribe` TEXT, `description` TEXT, `category` TEXT, `type` TEXT, `file` TEXT, `time` INTEGER DEFAULT (unixepoch()));
 SQL;
 
 $db->exec($query);
