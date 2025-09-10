c21b74724c5b062819118a11373365683a9a276f
ionvop <43801264+ionvop@users.noreply.github.com>
2025-09-10 09:50:47 +0800

    Update index.php

--- api/scan/index.php
+++ api/scan/index.php
@@ -7,40 +7,47 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 try {
-    if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
-        http_response_code(204);
-        exit;
-    }
-
     $db = new SQLite3("database.db");
     $_POST = json_decode(file_get_contents("php://input"), true);
 
-    $query = <<<SQL
-        SELECT * FROM `qr` WHERE `code` = :code
-    SQL;
+    switch ($_SERVER["REQUEST_METHOD"]) {
+        case "OPTIONS":
+            http_response_code(204);
+            exit;
+        case "POST":
+            $query = <<<SQL
+                SELECT * FROM `qr` WHERE `code` = :code
+            SQL;
 
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":code", $_POST["code"]);
-    $qr = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(":code", $_POST["code"]);
+            $qr = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
 
-    if ($qr == false) {
-        http_response_code(404);
-        echo json_encode(["error" => "QR code not found."]);
-        exit;
+            if ($qr == false) {
+                http_response_code(404);
+                echo json_encode(["error" => "QR code not found."]);
+                exit;
+            }
+
+            $query = <<<SQL
+                INSERT INTO `qr_scans` (`qr_id`)
+                VALUES (:qr_id);
+            SQL;
+
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(":qr_id", $qr["id"]);
+            $stmt->execute();
+
+            echo json_encode([
+                "success" => true
+            ]);
+
+            exit;
+        default:
+            http_response_code(405);
+            echo json_encode(["error" => "Method not allowed."]);
+            exit;
     }
-
-    $query = <<<SQL
-        INSERT INTO `qr_scans` (`qr_id`)
-        VALUES (:qr_id);
-    SQL;
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":qr_id", $qr["id"]);
-    $stmt->execute();
-
-    echo json_encode([
-        "success" => true
-    ]);
 } catch (Exception $e) {
     http_response_code(500);
     echo json_encode(["error" => $e->getMessage()]);
