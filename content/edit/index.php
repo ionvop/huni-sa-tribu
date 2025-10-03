c056e4041a0f05172f0225bc7dae719683679e6d
ionvop <ionvop@gmail.com>
2025-10-03 22:17:01 +0800

    Update index.php

--- content/edit/index.php
+++ content/edit/index.php
@@ -221,35 +221,13 @@ if ($content == false) {
                                 $stmt->bindValue(":id", $content["id"]);
                                 $qr = $stmt->execute()->fetchArray();
 
-                                if ($qr == false) {
-                                    echo <<<HTML
-                                        <button style="
-                                            background-color: #000a;"
-                                            name="method"
-                                            value="generateQr">
-                                            <div style="
-                                                display: grid;
-                                                grid-template-columns: repeat(2, max-content);">
-                                                <div style="
-                                                    display: flex;
-                                                    align-items: center;">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-560v-240q0-17 11.5-28.5T160-840h240q17 0 28.5 11.5T440-800v240q0 17-11.5 28.5T400-520H160q-17 0-28.5-11.5T120-560Zm80-40h160v-160H200v160Zm-80 440v-240q0-17 11.5-28.5T160-440h240q17 0 28.5 11.5T440-400v240q0 17-11.5 28.5T400-120H160q-17 0-28.5-11.5T120-160Zm80-40h160v-160H200v160Zm320-360v-240q0-17 11.5-28.5T560-840h240q17 0 28.5 11.5T840-800v240q0 17-11.5 28.5T800-520H560q-17 0-28.5-11.5T520-560Zm80-40h160v-160H600v160Zm160 480v-80h80v80h-80ZM520-360v-80h80v80h-80Zm80 80v-80h80v80h-80Zm-80 80v-80h80v80h-80Zm80 80v-80h80v80h-80Zm80-80v-80h80v80h-80Zm0-160v-80h80v80h-80Zm80 80v-80h80v80h-80Z"/></svg>
-                                                </div>
-                                                <div style="
-                                                    display: flex;
-                                                    align-items: center;
-                                                    padding-left: 1rem;">
-                                                    Generate QR
-                                                </div>
-                                            </div>
-                                        </button>
-                                    HTML;
-                                } else {
-                                    echo <<<HTML
-                                        <a href="visitors/qr/view?id={$qr['id']}">
+                                if ($content["category"] == "artifact" || $content["category"] == "event") {
+                                    if ($qr == false) {
+                                        echo <<<HTML
                                             <button style="
                                                 background-color: #000a;"
-                                                type="button">
+                                                name="method"
+                                                value="generateQr">
                                                 <div style="
                                                     display: grid;
                                                     grid-template-columns: repeat(2, max-content);">
@@ -262,12 +240,36 @@ if ($content == false) {
                                                         display: flex;
                                                         align-items: center;
                                                         padding-left: 1rem;">
-                                                        View QR
+                                                        Generate QR
                                                     </div>
                                                 </div>
                                             </button>
-                                        </a>
-                                    HTML;
+                                        HTML;
+                                    } else {
+                                        echo <<<HTML
+                                            <a href="visitors/qr/view?id={$qr['id']}">
+                                                <button style="
+                                                    background-color: #000a;"
+                                                    type="button">
+                                                    <div style="
+                                                        display: grid;
+                                                        grid-template-columns: repeat(2, max-content);">
+                                                        <div style="
+                                                            display: flex;
+                                                            align-items: center;">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-560v-240q0-17 11.5-28.5T160-840h240q17 0 28.5 11.5T440-800v240q0 17-11.5 28.5T400-520H160q-17 0-28.5-11.5T120-560Zm80-40h160v-160H200v160Zm-80 440v-240q0-17 11.5-28.5T160-440h240q17 0 28.5 11.5T440-400v240q0 17-11.5 28.5T400-120H160q-17 0-28.5-11.5T120-160Zm80-40h160v-160H200v160Zm320-360v-240q0-17 11.5-28.5T560-840h240q17 0 28.5 11.5T840-800v240q0 17-11.5 28.5T800-520H560q-17 0-28.5-11.5T520-560Zm80-40h160v-160H600v160Zm160 480v-80h80v80h-80ZM520-360v-80h80v80h-80Zm80 80v-80h80v80h-80Zm-80 80v-80h80v80h-80Zm80 80v-80h80v80h-80Zm80-80v-80h80v80h-80Zm0-160v-80h80v80h-80Zm80 80v-80h80v80h-80Z"/></svg>
+                                                        </div>
+                                                        <div style="
+                                                            display: flex;
+                                                            align-items: center;
+                                                            padding-left: 1rem;">
+                                                            View QR
+                                                        </div>
+                                                    </div>
+                                                </button>
+                                            </a>
+                                        HTML;
+                                    }
                                 }
                             ?>
                         </div>
